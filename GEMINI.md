58c5ab5db2be183c78c4d65628a1e0a4055331b1
Joshua McDonald <mcdonald.xman.joshua@gmail.com>
2025-07-18 15:30:50 -0500

    updated rules

--- GEMINI.md
+++ GEMINI.md
@@ -6,8 +6,5 @@
 **Node.js: use latest compatable with SvelteKit5**
 **Package Manager: Initialize with pnpm, but you can also use npm**
 
-### STRUCTURE
-Setup the strucutre of the files/project in a way a modern SvelteKit5 website would be.
-
 ### DEPLOYMENT
 Until this instruction changes all deployments of site should be done locally and can be run locally on windows. Eventual deployment will be done on Vercel.
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,22 +6,22 @@
 </script>
 
 <div class="flex flex-col min-h-screen w-full bg-[#FFFFFF]">
-  <div class="w-full bg-[#111111] flex justify-center">
-    <div class="w-[1440px] flex justify-center items-center flex-col gap-2.5 px-[180px]">
-      <Header />
+  <header class="w-full bg-[#111111] flex justify-center">
+    <Header />
+  </header>
+
+  <main class="flex-grow">
+    <div class="w-full bg-[#FFFFFF] flex justify-center">
+      <Hero />
     </div>
-  </div>
-  <div class="w-full bg-[#FFFFFF] flex justify-center">
-    <Hero />
-  </div>
-  <div class="w-full h-[4px] gradient-divider"></div>
-  <div class="w-full bg-[#FFFFFF] flex justify-center">
-    <CodeSamples />
-  </div>
-  <div class="w-full h-[4px] gradient-divider"></div>
-  <div class="w-full bg-[#F5F5F5] flex justify-center">
-    <div class="w-[1440px] flex justify-center items-center flex-col gap-2.5 px-[180px]">
-      <Footer />
+    <div class="w-full h-[4px] gradient-divider"></div>
+    <div class="w-full bg-[#FFFFFF] flex justify-center">
+      <CodeSamples />
     </div>
-  </div>
+    <div class="w-full h-[4px] gradient-divider"></div>
+  </main>
+
+  <footer class="w-full bg-[#F5F5F5] flex justify-center">
+    <Footer />
+  </footer>
 </div>
