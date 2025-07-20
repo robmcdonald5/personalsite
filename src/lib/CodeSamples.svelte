53df057734e8b20a714bffb0b871facf4bdcd5d2
Joshua McDonald <mcdonald.xman.joshua@gmail.com>
2025-07-19 21:05:21 -0500

    updated code samples showcase on main page

--- src/lib/CodeSamples.svelte
+++ src/lib/CodeSamples.svelte
@@ -1,27 +1,29 @@
-<section class="w-full max-w-[1440px] h-[656px] flex justify-start items-center flex-col gap-[90px] px-[5%] md:px-[180px] pt-[90px]">
-  <div class="flex justify-center items-center flex-row gap-[49px] w-full max-w-[1080px]">
-    <div class="flex-1 h-0 border-solid border-[#E0E0E0] border-b"></div>
-    <span class="text-[#000000] text-xl font-montserrat font-medium whitespace-nowrap">CODE SAMPLES</span>
-    <div class="flex-1 h-0 border-solid border-[#E0E0E0] border-b"></div>
-  </div>
-  <div class="flex justify-start items-center flex-row gap-[40px] md:gap-[120px] overflow-x-auto">
-    <div class="w-[280px] h-[240px] bg-[#FFFFFF] border-solid border-[#E0F7FA] border rounded-lg shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] relative">
-      <div class="text-[#000000] text-xl font-roboto font-light absolute left-[24px] top-[193px]">
-        Model Training
-      </div>
-      <div class="w-[264px] h-[160px] rounded-tl-md rounded-tr-md absolute left-[8px] top-[8px] card-image" style="background-image: url(/work-section__grid-showcase1-frame.png)"></div>
+<section class="w-full max-w-[1440px] h-[656px] flex justify-center items-center flex-col px-[5%] md:px-[180px]">
+  <div class="flex flex-col items-center gap-[90px] h-full justify-center">
+    <div class="flex justify-center items-center flex-row gap-[49px] w-full max-w-[1080px]">
+      <div class="flex-1 h-0 border-solid border-[#E0E0E0] border-b"></div>
+      <span class="text-[#000000] text-xl font-montserrat font-medium whitespace-nowrap">CODE SAMPLES</span>
+      <div class="flex-1 h-0 border-solid border-[#E0E0E0] border-b"></div>
     </div>
-    <div class="w-[280px] h-[240px] bg-[#FFFFFF] border-solid border-[#E0F7FA] border rounded-lg shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] relative">
-      <div class="text-[#000000] text-xl font-roboto font-light absolute left-[24px] top-[193px]">
-        API & Web Integration
+    <div class="flex justify-start items-center flex-row gap-[40px] md:gap-[120px] overflow-x-auto">
+      <div class="w-[280px] h-[240px] bg-[#FFFFFF] border-solid border-[#E0F7FA] border rounded-lg shadow-lg relative">
+        <div class="text-[#000000] text-xl font-roboto font-light absolute left-[24px] top-[193px]">
+          Model Training
+        </div>
+        <div class="w-[264px] h-[160px] rounded-tl-md rounded-tr-md absolute left-[8px] top-[8px] card-image" style="background-image: url(/work-section__grid-showcase1-frame.png)"></div>
       </div>
-      <div class="w-[264px] h-[160px] rounded-tl-md rounded-tr-md absolute left-[8px] top-[8px] card-image" style="background-image: url(/work-section__grid-showcase2-frame.png)"></div>
-    </div>
-    <div class="w-[280px] h-[240px] bg-[#FFFFFF] border-solid border-[#E0F7FA] border rounded-lg shadow-[0px_4px_4px_0px_rgba(0,0,0,0.25)] relative">
-      <div class="text-[#000000] text-xl font-roboto font-light absolute left-[24px] top-[193px]">
-        NLP & Preprocessing
+      <div class="w-[280px] h-[240px] bg-[#FFFFFF] border-solid border-[#E0F7FA] border rounded-lg shadow-lg relative">
+        <div class="text-[#000000] text-xl font-roboto font-light absolute left-[24px] top-[193px]">
+          API & Web Integration
+        </div>
+        <div class="w-[264px] h-[160px] rounded-tl-md rounded-tr-md absolute left-[8px] top-[8px] card-image" style="background-image: url(/work-section__grid-showcase2-frame.png)"></div>
       </div>
-      <div class="w-[264px] h-[160px] rounded-tl-md rounded-tr-md absolute left-[8px] top-[8px] card-image" style="background-image: url(/work-section__grid-showcase3-frame.png)"></div>
+      <div class="w-[280px] h-[240px] bg-[#FFFFFF] border-solid border-[#E0F7FA] border rounded-lg shadow-lg relative">
+        <div class="text-[#000000] text-xl font-roboto font-light absolute left-[24px] top-[193px]">
+          NLP & Preprocessing
+        </div>
+        <div class="w-[264px] h-[160px] rounded-tl-md rounded-tr-md absolute left-[8px] top-[8px] card-image" style="background-image: url(/work-section__grid-showcase3-frame.png)"></div>
+      </div>
     </div>
   </div>
 </section>
